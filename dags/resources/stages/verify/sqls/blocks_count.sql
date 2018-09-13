27f8c6176cf7f66ea33ec337f9677da6da051660
Evgeny Medvedev <medvedev1088@gmail.com>
2018-09-13 13:54:37 +0700

    Moved to https://github.com/blockchain-etl/ethereum-etl-airflow

--- dags/resources/stages/verify/sqls/blocks_count.sql
+++
@@ -1,4 +0,0 @@
-SELECT IF(
-(SELECT MAX(number) FROM `bigquery-public-data.ethereum_blockchain.blocks`) + 1 =
-(SELECT COUNT(*) FROM `bigquery-public-data.ethereum_blockchain.blocks`), 1,
-CAST((SELECT 'Total number of blocks except genesis is not equal to last block number {{ds}}') AS INT64))
--- dags/resources/stages/verify/sqls/logs_have_latest.sql
+++
@@ -1,3 +0,0 @@
-SELECT IF(
-(SELECT COUNT(*) FROM `bigquery-public-data.ethereum_blockchain.logs` WHERE DATE(block_timestamp) = '{{ds}}') > 0, 1,
-CAST((SELECT 'There are no logs on {{ds}}') AS INT64))
