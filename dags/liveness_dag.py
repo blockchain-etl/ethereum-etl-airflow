b96af2480d549b2c8b585640bc493738b95d9b38
Evgeny Medvedev <medvedev1088@gmail.com>
2018-10-01 23:02:45 +0700

    Add email notifications to liveness_dag.py

--- dags/liveness_dag.py
+++ dags/liveness_dag.py
@@ -1,3 +1,5 @@
+import os
+
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
@@ -6,8 +8,13 @@ from datetime import timedelta
 default_dag_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': airflow.utils.dates.days_ago(0)
+    'start_date': airflow.utils.dates.days_ago(0),
+    'email_on_failure': True,
 }
+
+notification_emails = os.environ.get('NOTIFICATION_EMAILS')
+if notification_emails and len(notification_emails) > 0:
+    default_dag_args['email'] = [email.strip() for email in notification_emails.split(',')]
 
 dag = DAG(
     'liveness_dag',
