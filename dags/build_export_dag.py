8b72f252224b655b8df0ea30b5e1db1b5a33694a
Evgeny Medvedev <medvedev1088@gmail.com>
2019-01-30 21:07:32 +0700

    Fix download_from_gcs for file of 0 size

--- dags/build_export_dag.py
+++ dags/build_export_dag.py
@@ -387,7 +387,12 @@ def download_from_gcs(bucket, object, filename):
     storage_client = storage.Client()
 
     bucket = storage_client.get_bucket(bucket)
-    blob = bucket.blob(object, chunk_size=10 * MEGABYTE)
+    blob_meta = bucket.get_blob(object)
+
+    if blob_meta.size > 0:
+        blob = bucket.blob(object, chunk_size=10 * MEGABYTE)
+    else:
+        blob = bucket.blob(object)
 
     blob.download_to_filename(filename)
 
