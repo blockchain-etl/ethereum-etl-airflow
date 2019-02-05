d5f78e4709a0a6d867e9e617d45379bfc34f8c1b
Webster <webster@iteriodata.com>
2019-02-05 13:14:56 -0800

    Add COPY options in load_dag_redshift.py; remove comments and unused imports

--- dags/load_dag_redshift.py
+++ dags/load_dag_redshift.py
@@ -1,17 +1,10 @@
 from __future__ import print_function
 
-import json
 import logging
 import os
-import time
 from datetime import datetime, timedelta
 
 from airflow import models
-from airflow.operators.email_operator import EmailOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.s3_to_redshift_operator import S3ToRedshiftTransfer
-from airflow.sensors.s3_key_sensor import S3KeySensor
-
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.operators.python_operator import PythonOperator
 
@@ -76,7 +69,7 @@ def load_task(ds, **kwargs):
 	    FROM 's3://{output_bucket}/export/{table}/block_date={date}/{table}.{file_format}'
 	    WITH CREDENTIALS
 	    'aws_access_key_id={aws_access_key_id};aws_secret_access_key={aws_secret_access_key}'
-	    json 'auto';
+	    TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL JSON 'auto';
 	"""
     else:
 	raise ValueError('Only json and csv file formats are supported.')
@@ -90,25 +83,10 @@ def load_task(ds, **kwargs):
 	aws_access_key_id=aws_access_key_id,
 	aws_secret_access_key=aws_secret_access_key
     )
-    result = pg_hook.run(formatted_sql)
-    print(str(result))
-    # assert
+    pg_hook.run(formatted_sql)
 
 
 def add_load_tasks(task, file_format, allow_quoted_newlines=False):
-
-    #wait_sensor = S3KeySensor(
-    #    task_id='wait_latest_{task}'.format(task=task),
-    #    dag=dag,
-    #    bucket_name=output_bucket,
-    #    bucket_key='export/{task}/block_date={datestamp}/{task}.{file_format}'.format(
-    #        task=task, datestamp='{{ds}}', file_format=file_format),
-    #    wildcard_match=False,
-    #    aws_conn_id='aws_default',
-    #    verify=None,
-    #    timeout=60 * 60,
-    #    poke_interval=60
-    #)
 
     load_operator = PythonOperator(
 	task_id='s3_to_redshift_{task}'.format(task=task),
@@ -122,7 +100,6 @@ def add_load_tasks(task, file_format, allow_quoted_newlines=False):
 	},
     )
 
-    #wait_sensor >> load_operator
     return load_operator
 
 
@@ -133,4 +110,3 @@ load_logs_task = add_load_tasks('logs', 'json')
 load_contracts_task = add_load_tasks('contracts', 'json')
 load_tokens_task = add_load_tasks('tokens', 'csv', allow_quoted_newlines=True)
 load_token_transfers_task = add_load_tasks('token_transfers', 'csv')
-load_traces_task = add_load_tasks('traces', 'csv')
