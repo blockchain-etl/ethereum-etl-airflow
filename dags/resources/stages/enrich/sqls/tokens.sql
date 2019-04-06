412ae2f13cabd1583c9a5f5cab14ec88ad1c9600
Evgeny Medvedev <medvedev1088@gmail.com>
2019-04-06 20:20:42 +0700

    Update load_dag

--- dags/resources/stages/enrich/sqls/tokens.sql
+++ dags/resources/stages/enrich/sqls/tokens.sql
@@ -1,18 +1,8 @@
-WITH tokens_grouped AS (
-    SELECT
-        address,
-        symbol,
-        name,
-        decimals,
-        total_supply,
-        ROW_NUMBER() OVER (PARTITION BY address) AS rank
-    FROM
-        {{DATASET_NAME_RAW}}.tokens)
 SELECT
     address,
     symbol,
     name,
     decimals,
     total_supply
-FROM tokens_grouped
-WHERE tokens_grouped.rank = 1
+FROM {{DATASET_NAME_RAW}}.tokens
+
