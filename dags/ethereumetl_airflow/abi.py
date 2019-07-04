03d6b6300da7b26977927929323b0c18318ffd04
Evgeny Medvedev <medvedev1088@gmail.com>
2019-07-04 18:46:37 +0700

    Add export_all_contract_codes_dag.py

--- dags/ethereumetl_airflow/abi.py
+++ dags/ethereumetl_airflow/abi.py
@@ -3,9 +3,10 @@ from random import shuffle
 
 import requests
 
+all_tokens = ['token1', 'token2']
 
 def get_contract_abi(address):
-    all_tokens = ['token', 'token2']
+
     shuffle(all_tokens)
     response = requests.get(
         "https://api.etherscan.io/api?module=contract&action=getabi&address={address}&apikey={token}".format(
@@ -24,3 +25,24 @@ def get_contract_abi(address):
         raise ValueError('status in response is not 0 or 1 ' + json.dumps(parsed_content))
 
     return parsed_content['result']
+
+
+def get_contract_code(address):
+    shuffle(all_tokens)
+    response = requests.get(
+        "https://api.etherscan.io/api?module=contract&action=getsourcecode&address={address}&apikey={token}".format(
+            address=address, token=all_tokens[0]
+        ))
+
+    response.raise_for_status()
+
+    content = response.content
+
+    parsed_content = json.loads(content)
+
+    if parsed_content['status'] == '0':
+        return ''
+    elif parsed_content['status'] != '1':
+        raise ValueError('status in response is not 0 or 1 ' + json.dumps(parsed_content))
+
+    return content
