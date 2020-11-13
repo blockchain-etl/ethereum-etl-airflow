8f7e98a5f42611caf81b566ab6db50a6b3208928
medvedev1088 <medvedev1088@gmail.com>
2020-11-13 17:01:47 +0700

    Verify partitioned traces - use multiple partitioned tables

--- dags/resources/stages/verify_streaming/sqls/partitioned_traces_have_latest.sql
+++ dags/resources/stages/verify_streaming/sqls/partitioned_traces_have_latest.sql
@@ -1,22 +1,22 @@
 with top_partitioned_traces as (
   select block_timestamp
-  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_to_address_0x7f6`
+  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_input_0xa90`
   where date(block_timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
   union all
   select block_timestamp
-  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_to_address_0xdac`
+  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_input_0x70a`
   where date(block_timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
   union all
   select block_timestamp
-  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_to_address_0xc02`
+  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_input_0x23b`
   where date(block_timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
   union all
   select block_timestamp
-  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_to_address_0x000`
+  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_input_0x090`
   where date(block_timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
   union all
   select block_timestamp
-  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_to_address_0xa3c`
+  from `{{params.internal_project_id}}.{{params.partitioned_dataset_name}}.traces_by_input_0x022`
   where date(block_timestamp) >= date_add('{{ds}}', INTERVAL -1 DAY)
 )
 select if(
