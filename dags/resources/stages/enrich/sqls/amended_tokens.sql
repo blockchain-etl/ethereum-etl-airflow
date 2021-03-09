429ef62e827a70f7c2054f8841ca4b307d9b3be9
medvedev1088 <medvedev1088@gmail.com>
2021-03-09 13:02:17 +0700

    Fix token_amendments.csv

--- dags/resources/stages/enrich/sqls/amended_tokens.sql
+++ dags/resources/stages/enrich/sqls/amended_tokens.sql
@@ -7,14 +7,24 @@ WITH tokens AS (
         ANY_VALUE(decimals) AS decimals,
     FROM `bigquery-public-data.crypto_ethereum.tokens`
     GROUP BY address
+),
+deduplicated_token_amendments AS (
+    -- Deduplicate first since token_amendments can have human errors
+    SELECT
+        address,
+        ANY_VALUE(symbol) AS symbol,
+        ANY_VALUE(name) AS name,
+        ANY_VALUE(decimals) AS decimals,
+    FROM `blockchain-etl-internal.common.token_amendments`
+    GROUP BY address
 )
 SELECT 
-    address,
+    LOWER(address),
     COALESCE(am.symbol, tokens.symbol) AS symbol,
     COALESCE(am.name, tokens.name) AS name,
     COALESCE(am.decimals, tokens.decimals) AS decimals,
 FROM
-  `blockchain-etl-internal.common.token_amendments` AS am
+  deduplicated_token_amendments AS am
 FULL OUTER JOIN
   tokens
 USING(address)
