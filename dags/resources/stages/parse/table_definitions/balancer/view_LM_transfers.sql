97babb9d3ec7c23354fd845db5c3d5e633d24735
Markus Buhatem Koch <markusbkoch@gmail.com>
2022-03-29 16:25:13 +0100

    add veBAL to LM transfers view

--- dags/resources/stages/parse/table_definitions/balancer/view_LM_transfers.sql
+++ dags/resources/stages/parse/table_definitions/balancer/view_LM_transfers.sql
@@ -5,6 +5,7 @@
 -- On Mainnet, Liquidity Mining Power is derived from V2 BPTs plus:
 --   1. Aave's staking module
 --   2. GRO's staking contract
+--   3. BPT locked as veBAL
 
 -- V1 stkABPT transfers
 select 
@@ -25,6 +26,30 @@ select
   block_number,
   block_timestamp
 FROM `blockchain-etl.ethereum_balancer.V2_BalancerPoolToken_event_Transfer` 
+union all
+-- veBAL - 80/20 BPT deposits
+-- are handled as trasnfers back to the user
+-- so that the token transfer and the deposit cancel each other out
+select
+  '0x5c6ee304399dbdb9c8ef030ab642b10820db8f56' as token_address,
+  '0xc128a9954e6c874ea3d62ce62b468ba073093f25' as from_address,
+  provider as to_address,
+  value,
+  block_number,
+  block_timestamp
+FROM `blockchain-etl.ethereum_balancer.VotingEscrow_event_Deposit` 
+union all
+-- veBAL - 80/20 BPT deposits withdrawals
+-- are handled as transfers back to the veBAL contract
+-- so that the token transfer and the withdraw cancel each other out
+select
+  '0x5c6ee304399dbdb9c8ef030ab642b10820db8f56' as token_address,
+  provider as from_address,
+  '0xc128a9954e6c874ea3d62ce62b468ba073093f25' as to_address,
+  value,
+  block_number,
+  block_timestamp
+FROM `blockchain-etl.ethereum_balancer.VotingEscrow_event_Withdraw` 
 union all
 -- 80/20 GRO pool staking contract deposits
 -- are handled as trasnfers back to the user
