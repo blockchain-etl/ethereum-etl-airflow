81eaaaf5b5c7ce51ca5a80812af0806a91382b45
medvedev1088 <medvedev1088@gmail.com>
2023-05-24 11:36:00 +0800

    Include cashPrior column after review

--- dags/resources/stages/parse/table_definitions/compound/cToken_event_AccrueInterest_combined.sql
+++ dags/resources/stages/parse/table_definitions/compound/cToken_event_AccrueInterest_combined.sql
@@ -1,24 +1,24 @@
 -- AccrueInterest for some tokens don't include the cashPrior field. This view combines events with both signatures
-SELECT * except(cashPrior)
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, cashPrior, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cToken_event_AccrueInterest` 
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cUSDC_event_AccrueInterest`
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cREP_event_AccrueInterest`
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cBAT_event_AccrueInterest`
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cETH_event_AccrueInterest`
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cSAI_event_AccrueInterest`
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cWBTC_event_AccrueInterest`
 UNION ALL
-SELECT *
+SELECT block_timestamp, block_number, transaction_hash, log_index, contract_address, null, interestAccumulated, borrowIndex, totalBorrows
 FROM `blockchain-etl.ethereum_compound.cZRX_event_AccrueInterest`
