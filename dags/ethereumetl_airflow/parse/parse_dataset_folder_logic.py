c005dd2a32159857b36a340983f8e43ffc3e53f2
medvedev1088 <medvedev1088@gmail.com>
2023-07-27 21:16:52 +0800

    Add _last_ds and _version to parse state

--- dags/ethereumetl_airflow/parse/parse_dataset_folder_logic.py
+++ dags/ethereumetl_airflow/parse/parse_dataset_folder_logic.py
@@ -3,44 +3,77 @@ import os
 import time
 
 from ethereumetl_airflow.bigquery_utils import create_view
-from ethereumetl_airflow.common import read_json_file, read_file
+from ethereumetl_airflow.common import read_file, read_json_file
 from ethereumetl_airflow.parse.parse_state_manager import ParseStateManager
-from ethereumetl_airflow.parse.parse_table_definition_logic import parse, ref_regex, create_dataset, replace_refs
+from ethereumetl_airflow.parse.parse_table_definition_logic import (
+    create_dataset,
+    parse,
+    ref_regex,
+    replace_refs,
+)
 from ethereumetl_airflow.parse.table_definition import TableDefinitionFileType
-from ethereumetl_airflow.parse.table_definition_reader import read_table_definitions, toposort_and_read_table_definition_states
+from ethereumetl_airflow.parse.table_definition_reader import (
+    read_table_definitions,
+    toposort_and_read_table_definition_states,
+)
 
 
 def parse_dataset_folder(
-        bigquery_client,
-        dataset_folder,
-        ds,
-        source_project_id,
-        source_dataset_name,
-        destination_project_id,
-        parse_state_manager: ParseStateManager,
-        sqls_folder,
-        parse_all_partitions,
-        time_func=time.time
+    bigquery_client,
+    dataset_folder,
+    ds,
+    source_project_id,
+    source_dataset_name,
+    destination_project_id,
+    parse_state_manager: ParseStateManager,
+    sqls_folder,
+    parse_all_partitions,
+    time_func=time.time,
+    only_updated=False,
 ):
-    logging.info(f'Parsing dataset folder {dataset_folder}')
+    logging.info(f"Parsing dataset folder {dataset_folder}")
+
+    if ds is None:
+        ds = parse_state_manager.get_last_ds()
 
     table_definitions = read_table_definitions(dataset_folder)
 
-    table_definition_states = toposort_and_read_table_definition_states(table_definitions, parse_state_manager)
+    table_definition_states = toposort_and_read_table_definition_states(
+        table_definitions, parse_state_manager
+    )
 
     for table_definition_state in table_definition_states:
-        logging.info(f'{table_definition_state.table_definition.table_name}: is_updated_or_dependencies_updated: {table_definition_state.is_updated_or_dependencies_updated}')
+        logging.info(
+            f"{table_definition_state.table_definition.table_name}: is_updated_or_dependencies_updated: {table_definition_state.is_updated_or_dependencies_updated}"
+        )
 
-    updated_table_definitions = [tds.table_definition for tds in table_definition_states if tds.is_updated_or_dependencies_updated]
+    updated_table_definitions = [
+        tds.table_definition
+        for tds in table_definition_states
+        if tds.is_updated_or_dependencies_updated
+    ]
 
     # Prevents accidentally running full refresh for many tables caused by bugs
     max_num_updated_table_definitions = 50
     if len(updated_table_definitions) > max_num_updated_table_definitions:
-        raise ValueError(f'More than {max_num_updated_table_definitions} will be fully refreshed. Please make sure not more than {max_num_updated_table_definitions} are updated')
+        raise ValueError(
+            f"More than {max_num_updated_table_definitions} will be fully refreshed. Please make sure not more than {max_num_updated_table_definitions} are updated"
+        )
 
     for index, table_definition_state in enumerate(table_definition_states):
         table_definition = table_definition_state.table_definition
-        logging.info(f'Parsing file {index} out of {len(table_definition_states)}: {table_definition.filepath}')
+        if (
+            only_updated
+            and not table_definition_state.is_updated_or_dependencies_updated
+        ):
+            logging.info(
+                f"Skipping file {index} out of {len(table_definition_states)}: {table_definition.filepath}"
+            )
+            continue
+
+        logging.info(
+            f"Parsing file {index} out of {len(table_definition_states)}: {table_definition.filepath}"
+        )
         if table_definition.filetype == TableDefinitionFileType.JSON:
             table_definition_content = read_json_file(table_definition.filepath)
             parse(
@@ -51,29 +84,37 @@ def parse_dataset_folder(
                 source_dataset_name,
                 destination_project_id,
                 sqls_folder,
-                parse_all_partitions if parse_all_partitions is not None else table_definition_state.is_updated_or_dependencies_updated,
-                time_func=time_func
+                parse_all_partitions
+                if parse_all_partitions is not None
+                else table_definition_state.is_updated_or_dependencies_updated,
+                time_func=time_func,
             )
         elif table_definition.filetype == TableDefinitionFileType.SQL:
             view_name = table_definition.table_name
             dest_table_name = view_name
 
             dataset_name = os.path.basename(dataset_folder)
-            dataset_name = 'ethereum_' + dataset_name
+            dataset_name = "ethereum_" + dataset_name
 
-            dest_table_ref = create_dataset(bigquery_client, dataset_name, destination_project_id).table(dest_table_name)
+            dest_table_ref = create_dataset(
+                bigquery_client, dataset_name, destination_project_id
+            ).table(dest_table_name)
             sql = read_file(table_definition.filepath)
             sql = replace_refs(sql, ref_regex, destination_project_id, dataset_name)
 
             create_view(bigquery_client, sql, dest_table_ref)
         else:
-            raise ValueError(f'Unrecognized file type: {table_definition.filepath}')
+            raise ValueError(f"Unrecognized file type: {table_definition.filepath}")
 
     for table_definition_state in table_definition_states:
         if table_definition_state.is_updated_or_dependencies_updated:
             table_definition = table_definition_state.table_definition
-            logging.info(f'Updating content hash for {table_definition.table_name}: {table_definition.content_hash}')
-            parse_state_manager.set_content_hash(table_definition.table_name, table_definition.content_hash)
+            logging.info(
+                f"Updating content hash for {table_definition.table_name}: {table_definition.content_hash}"
+            )
+            parse_state_manager.set_content_hash(
+                table_definition.table_name, table_definition.content_hash
+            )
 
+    parse_state_manager.set_last_ds(ds)
     parse_state_manager.persist_state()
-
--- dags/ethereumetl_airflow/parse/parse_state_manager.py
+++ dags/ethereumetl_airflow/parse/parse_state_manager.py
@@ -1,17 +1,21 @@
 import json
 import logging
+import time
 
-from google.cloud.exceptions import NotFound
 from google.cloud import storage
+from google.cloud.exceptions import NotFound
 
 
 class ParseStateManager:
-    def __init__(self, dataset_name, state_bucket, bucket_path):
+    def __init__(self, dataset_name, state_bucket, bucket_path, project=None):
         self.dataset_name = dataset_name
         self.state_bucket = state_bucket
         self.bucket_path = bucket_path
 
-        self.storage_client = storage.Client()
+        if project is not None:
+            self.storage_client = storage.Client(project=project)
+        else:
+            self.storage_client = storage.Client()
 
         state_file = self._download_state_file()
         if state_file:
@@ -22,21 +26,33 @@ class ParseStateManager:
     def get_content_hash(self, table_name):
         content_hash = self.state.get(table_name)
         if not content_hash:
-            content_hash = ''
+            content_hash = ""
         return content_hash
 
     def set_content_hash(self, table_name, new_hash):
         self.state[table_name] = new_hash
 
+    def get_last_ds(self):
+        last_ds = self.state.get("_last_ds")
+        if last_ds is None:
+            raise ValueError("_last_ds is None in parse state")
+        return last_ds
+
+    def set_last_ds(self, ds):
+        self.state["_last_ds"] = ds
+
     def persist_state(self):
+        self._check_version()
+        self._set_new_version()
+
         bucket = self.storage_client.get_bucket(self.state_bucket)
         blob = bucket.blob(self._build_state_file_name())
         state_str = json.dumps(self.state)
-        logging.info(f'Persisting parse state: {state_str}')
+        logging.info(f"Persisting parse state: {state_str}")
         blob.upload_from_string(state_str)
 
     def _build_state_file_name(self):
-        return f'{self.bucket_path}/{self.dataset_name}/state.json'
+        return f"{self.bucket_path}/{self.dataset_name}/state.json"
 
     def _download_state_file(self):
         bucket = self.storage_client.get_bucket(self.state_bucket)
@@ -48,3 +64,29 @@ class ParseStateManager:
             return content
         except NotFound:
             return None
+
+    def _check_version(self):
+        # Optimistic locking to prevent race conditions with CI/CD
+        state_file = self._download_state_file()
+        if not state_file:
+            return
+        else:
+            state = json.loads(state_file)
+
+        persisted_version = state.get("_version")
+        if not persisted_version:
+            return
+
+        current_version = self.state.get("_version")
+        if persisted_version != current_version:
+            raise ValueError(
+                f"The state version on the bucket is different from the version in memory,"
+                f"bucket: {persisted_version}, memory: {current_version}. "
+                f"Another process must have run in parallel"
+            )
+
+    def _set_new_version(self):
+        new_version = int(
+            time.time() * 1000
+        )  # time.time() returns seconds, so we multiply by 1000 for milliseconds
+        self.state["_version"] = str(new_version)
