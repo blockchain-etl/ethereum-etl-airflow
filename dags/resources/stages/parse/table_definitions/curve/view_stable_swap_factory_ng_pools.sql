41de7dda210884bb738f7ec82aaf7d2c37f5b645
zigzag2002 <luke.xie@gauntlet.xyz>
2024-04-26 10:33:41 -0700

    Update view_stable_swap_factory_ng_pools.sql

--- dags/resources/stages/parse/table_definitions/curve/view_stable_swap_factory_ng_pools.sql
+++ dags/resources/stages/parse/table_definitions/curve/view_stable_swap_factory_ng_pools.sql
@@ -6,3 +6,4 @@ WHERE
   from_address = '0x0000000000000000000000000000000000000000'
   AND to_address = '0x6a8cbed756804b16e05e741edabd5cb544ae21bf'
   AND value = '0'
+  AND TIMESTAMP_TRUNC(block_timestamp, day) > TIMESTAMP('2023-10-29')
