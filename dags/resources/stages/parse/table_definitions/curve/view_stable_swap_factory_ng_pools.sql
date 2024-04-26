eb879fd562851591733d9c867fb90a59e146340a
zigzag2002 <luke.xie@gauntlet.xyz>
2024-04-25 19:43:45 -0700

    create view for pools

---
+++ dags/resources/stages/parse/table_definitions/curve/view_stable_swap_factory_ng_pools.sql
@@ -0,0 +1,8 @@
+SELECT
+  token_address
+FROM
+  `bigquery-public-data.crypto_ethereum.token_transfers`
+WHERE
+  from_address = '0x0000000000000000000000000000000000000000'
+  AND to_address = '0x6a8cbed756804b16e05e741edabd5cb544ae21bf'
+  AND value = '0'
