9726660c400a690f515f9013e20bc4652268234b
Akshay <araa@connect.ust.hk>
2024-07-10 13:13:11 +0800

    Add trace parsing support

--- generate_parse_sql.py
+++ generate_parse_sql.py
@@ -20,8 +20,19 @@ def generate_sql_from_json(json_file_path, date):
 
     json_string = json.dumps(data).replace("'", "''")
     signature = calculate_signature(data['parser']['abi'])
+    parser_type = data['parser']['type']
 
-    sql = f"""
+    if parser_type == 'log':
+        sql = generate_log_sql(json_string, signature, date)
+    elif parser_type == 'trace':
+        sql = generate_trace_sql(json_string, signature, date)
+    else:
+        raise ValueError(f"Unsupported parser type: {parser_type}")
+
+    return sql
+
+def generate_log_sql(json_string, signature, date):
+    return f"""
 WITH
 abi AS 
 (
@@ -67,7 +78,53 @@ SELECT * FROM logs
 LIMIT 100
 """
 
-    return sql
+def generate_trace_sql(json_string, signature, date):
+    return f"""
+WITH
+abi AS 
+(
+SELECT
+    JSON_QUERY(json_data, '$.parser.abi') AS abi,
+    JSON_QUERY(json_data, '$.parser.field_mapping') AS field_mapping,
+    JSON_QUERY(json_data, '$.table') AS table_details,
+    JSON_EXTRACT_SCALAR(json_data, '$.parser.contract_address') AS contract_address,
+    CAST(JSON_EXTRACT_SCALAR(json_data, '$.parser.type') AS STRING) AS parser_type
+FROM (
+    SELECT '{json_string}' AS json_data
+)
+),
+
+details AS (
+    SELECT 
+        '{signature}' AS sig,
+        abi.*
+    FROM abi
+),
+
+traces AS (
+  SELECT
+    t.*,
+    a.sig, 
+    `blockchain-etl-internal.common.parse_trace`(
+            t.input,
+            REPLACE(a.abi, "'", '"')
+        ) AS parsed_trace
+  FROM
+    `bigquery-public-data.crypto_ethereum.traces` AS t
+  INNER JOIN
+        details AS a
+    ON
+        STARTS_WITH(t.input, a.sig)
+  WHERE
+    DATE(t.block_timestamp) = DATE("{date}")
+    AND (a.contract_address IS NULL OR t.to_address = LOWER(a.contract_address))
+    AND t.status = 1  -- Only successful calls
+    AND t.call_type = 'call'  -- Only direct calls
+)
+
+SELECT * FROM traces
+LIMIT 100
+"""
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
